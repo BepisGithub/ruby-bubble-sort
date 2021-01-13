c0c3d7c7450ff0297265384f2064b89ebe6c9f0c
bepis <luqdek@gmail.com>
2021-01-13 10:17:13 +0000

    Made the bubble sort run length-1 times to ensure everything gets sorted. Then iterated through each value in the array, if the value was more it would move it to the left so it sorts by size biggest to smallest

--- script.rb
+++ script.rb
@@ -1,9 +1,16 @@
+require "pry"
 def bubble_sort(arr)
     n = arr.length-1
-    arr.each_with_index do |val,index|
-        
+    n.times do 
+        arr.each_with_index do |val,index|
+            next_index = index+1
+            if arr[next_index] != nil && val < arr[next_index]
+                arr[index] = arr[next_index]
+                arr[next_index] = val
+            end
+        end
     end
-
+    p arr
 
 end
 
